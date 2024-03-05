729ae04632e5b0e0892c7c64e4ca59f296d977b7
Mikolaj <kazimierczakmikolaj@gmail.com>
2024-03-05 23:47:07 +0100

    start with flex layout implementation

--- frontend/src/layouts/FlexLayout.ts
+++ frontend/src/layouts/FlexLayout.ts
@@ -1,3 +1,36 @@
-class FlexLayout {}
+import * as THREE from "three";
 
-export default FlexLayout;
+enum FlexDirection {
+  Column,
+  Row,
+}
+
+enum FlexGrow {}
+
+type FlexLayoutElement = {
+  width: number;
+  height: number;
+  group: THREE.Group;
+};
+
+class FlexLayout {
+  constructor(
+    private direction: FlexDirection = FlexDirection.Row,
+    private width: number,
+    private height: number,
+    private children: FlexLayoutElement[]
+  ) {}
+
+  recalculate() {
+    if (this.direction == FlexDirection.Row) {
+      let startPositionX = 0;
+      this.children.forEach((child) => {
+        child.group.position.setX(startPositionX);
+        startPositionX += child.width;
+      });
+    } else {
+    }
+  }
+}
+
+export { FlexLayout, FlexDirection, type FlexLayoutElement };
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import { FlexLayout, FlexDirection } from "./layouts/FlexLayout";
 
 const gui = new GUI();
 const scene = new THREE.Scene();
@@ -18,11 +19,17 @@ controls.listenToKeyEvents(window);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-gui.add(cube.position, "y");
+const cubes = [
+  { width: 2, height: 1, group: createCube() },
+  { width: 1, height: 1, group: createCube() },
+];
+
+const cubesLayout = new FlexLayout(FlexDirection.Row, 100, 100, cubes);
+cubes.forEach((cube) => {
+  console.log(cube.group);
+  scene.add(cube.group);
+});
+cubesLayout.recalculate();
 camera.position.z = 5;
 
 function animate() {
@@ -32,3 +39,14 @@ function animate() {
 }
 
 animate();
+
+function createCube(
+  sizeX: number = 1,
+  sizeY: number = 1,
+  sizeZ: number = 1
+): THREE.Group {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  return new THREE.Group().add(cube);
+}
