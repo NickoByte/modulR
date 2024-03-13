5d2c421af9db798867537fe1cb9c6949073cd5b3
Mikolaj <kazimierczakmikolaj@gmail.com>
2024-03-13 23:58:17 +0100

    replace testing framework

--- frontend/src/layouts/AutoLayout.test.ts
+++ frontend/src/layouts/AutoLayout.test.ts
@@ -1,17 +1,26 @@
-import { AlignElements, AutoLayout, JustifyElements, LayoutDirection, LayoutElement } from "./AutoLayout";
+import {
+  AlignElements,
+  AutoLayout,
+  JustifyElements,
+  LayoutDirection,
+  LayoutElement,
+} from "./AutoLayout";
 import { Size } from "./Sizes";
 import * as THREE from "three";
+import { expect, test } from "vitest";
 
 test("vertical auto layout with nested auto layout", () => {
-  const rootLayout = new AutoLayout(Size.Unit(10), Size.Unit(10), new THREE.Group(),
+  const rootLayout = new AutoLayout(
+    Size.Unit(10),
+    Size.Unit(10),
+    new THREE.Group(),
     {
       direction: LayoutDirection.Row,
       alignElements: AlignElements.Stretch,
       justifyElements: JustifyElements.SpaceEvenly,
     },
-    [
-      new LayoutElement(Size.Unit(1), Size.Unit(1), new THREE.Object3D())
-    ]);
+    [new LayoutElement(Size.Unit(1), Size.Unit(1), new THREE.Object3D())]
+  );
 
   rootLayout.recalculate();
 
